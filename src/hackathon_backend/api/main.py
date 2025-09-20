3bc87cab740917bde192cd842fb59afe512d1273
Anderson Sánchez <anderson2002y@gmail.com>
2025-09-19 20:40:58 -0600

    feat(api): add routers for roles, me, and clinics

--- src/hackathon_backend/api/main.py
+++ src/hackathon_backend/api/main.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
-from hackathon_backend.api.routes import users
 from hackathon_backend.api.routes import auth
+from hackathon_backend.api.routes import users
+from hackathon_backend.api.routes import me
+from hackathon_backend.api.routes import roles
+from hackathon_backend.api.routes import clinics
 
 api_router = APIRouter()
+
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
+api_router.include_router(me.router)
+api_router.include_router(roles.router)
+api_router.include_router(clinics.router)

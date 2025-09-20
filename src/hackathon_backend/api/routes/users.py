1289be758cd004ef68fba3512d4a30f82f1db45e
Anderson Sánchez <anderson2002y@gmail.com>
2025-09-19 20:46:50 -0600

    feat(users): include roles in user list endpoint

--- src/hackathon_backend/api/routes/users.py
+++ src/hackathon_backend/api/routes/users.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from hackathon_backend.core.db import engine
 from hackathon_backend.models.user import User
+from hackathon_backend.models.user_clinic_role import UserClinicRole
 from hackathon_backend.schemas.api_response import ApiResponse
 from hackathon_backend.schemas.user import UserGet, UsersResponse
+from sqlalchemy.orm import selectinload
 
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -19,12 +22,24 @@ def get_session():
 def read_users_test(
     session: Session = Depends(get_session),
 ) -> ApiResponse[UsersResponse]:
-    """
-    Example endpoint: Retrieve all users.
-    """
-    statement = select(User)
+    
+    statement = select(User).options(
+        selectinload(User.user_clinic_roles).selectinload(UserClinicRole.role)
+    )
     users = session.exec(statement).all()
-    users_data = [UserGet.model_validate(u) for u in users]
+
+    if not users:
+        raise HTTPException(status_code=404, detail="Users not found")
+
+    users_data: List[UserGet] = [
+        UserGet.model_validate(
+            user,
+            update={
+                "roles": [ucr.role.name for ucr in user.user_clinic_roles if ucr.role]
+            },
+        )
+        for user in users
+    ]
 
     return ApiResponse(
         success=True,

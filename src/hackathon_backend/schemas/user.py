d686cbd299329ffd81cbe1426798af33c4cc26b9
Anderson Sánchez <anderson2002y@gmail.com>
2025-09-19 18:54:10 -0600

    refactor(user-schemas): restructure User schemas with separate input/output

--- src/hackathon_backend/schemas/user.py
+++ src/hackathon_backend/schemas/user.py
@@ -1,18 +1,48 @@
 from typing import List
+from datetime import datetime
 from pydantic import EmailStr
 from sqlmodel import SQLModel
 
+from hackathon_backend.schemas.clinic import ClinicRole
 
-class UserGet(SQLModel):
-    id: int
+
+class UserBase(SQLModel):
     first_name: str
     middle_name: str
-    last_nameL: str
+    last_name: str
     second_last_name: str
     email: EmailStr
     age: int
-    is_active: bool
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserGet(UserBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: datetime | None
+    roles: List[str] | None = []
+
+
+class UserResponse(SQLModel):
+    user: UserGet
 
 
 class UsersResponse(SQLModel):
     users: List[UserGet]
+
+
+class UserMe(UserBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    clinics: List[ClinicRole] = []
+
+
+
+class UserMeResponse(SQLModel):
+    user: UserMe   
+

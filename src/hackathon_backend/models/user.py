37b067c9286cf7b4371caf16a27edbe6bb23a7ba
Anderson Sánchez <anderson2002y@gmail.com>
2025-09-19 18:53:41 -0600

    feat(user-model): add mixins and field validations

--- src/hackathon_backend/models/user.py
+++ src/hackathon_backend/models/user.py
@@ -1,14 +1,18 @@
 from pydantic import EmailStr
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
+from hackathon_backend.models.mixins import TimestampMixin, SoftDeleteMixin, event
 
 
-class User(SQLModel, table=True):
+class User(SQLModel, TimestampMixin, SoftDeleteMixin, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    first_name: str
-    middle_name: str | None
-    last_name: str
-    second_last_name: str | None
-    email: EmailStr
-    age: int
+    first_name: str = Field(min_length=1, max_length=50)
+    middle_name: str | None = Field(default=None, max_length=50)
+    last_name: str = Field(min_length=1, max_length=50)
+    second_last_name: str | None = Field(default=None, max_length=50)
+    email: EmailStr = Field(index=True, unique=True)
+    age: int = Field(ge=0, le=120)
     hashed_password: str
-    is_active: bool = True
+
+    user_clinic_roles: list["UserClinicRole"] = Relationship(
+        back_populates="user",
+    )

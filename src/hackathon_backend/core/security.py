4a2dd9bde294ca54d309a3e8ecb7b0a85c3e7ff9
Anderson Sánchez <anderson2002y@gmail.com>
2025-09-20 15:48:36 -0600

    feat(security): add JWT user extraction and current user

--- src/hackathon_backend/core/security.py
+++ src/hackathon_backend/core/security.py
@@ -2,10 +2,18 @@ from datetime import datetime, timedelta, timezone
 from typing import Any
 import jwt
 from passlib.context import CryptContext
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+
 from hackathon_backend.core.config import settings
+from hackathon_backend.core.deps import get_db
+from hackathon_backend.models.user import User
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = "HS256"
+
+oauth2_scheme = OAuth2PasswordBearer(f"{settings.API_V1_STR}/auth/login")
 
 
 def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
@@ -17,3 +25,33 @@ def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def extract_user_id(token: str) -> int:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = int(payload.get("sub"))
+        if user_id is None:
+            raise ValueError("User ID not found in token")
+        return user_id
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except jwt.PyJWTError as e:
+        print("Error PyJWT:", e)
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), session: Session = Depends(get_db)
+) -> User:
+    """
+    Return the user athenticate form JWT.
+    """
+    print("=== Token recibido en get_current_user ===")
+    print(repr(token))
+    print("=========================================")
+    user_id = extract_user_id(token)
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+    return user
